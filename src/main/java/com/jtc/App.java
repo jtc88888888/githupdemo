3cc15b32302218b841f7e419618f700a3826b5ff
jiatengchao <710474777@qq.com>
2020-05-02 17:31:09 +0800

    jtc hahah

--- src/main/java/com/jtc/App.java
+++ src/main/java/com/jtc/App.java
@@ -1,5 +1,7 @@
 package com.jtc;
 
+import com.jtc.service.TestService;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,8 @@ public class App
     public static void main( String[] args )
 
     {
+        TestService ts=new TestService();
+        System.out.println(ts.getme());
         System.out.println( "哈哈哈哈哈" );
     }
 }
---
+++ src/main/java/com/jtc/service/TestService.java
@@ -0,0 +1,7 @@
+package com.jtc.service;
+
+public class TestService {
+    public String getme(){
+        return "my name is jtc";
+    }
+}
